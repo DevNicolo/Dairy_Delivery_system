131a233d74534340cdef5aca79210439830ba6ea
DevNicolo <dev.nicoloagricola@gmail.com>
2026-04-02 16:55:21 +0200

    fix: change public id in localhost to ensure portability

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -6,7 +6,7 @@
 //per non ricevere errori sul codice
 export const environment = {
   production: false,
-  baseUrl: 'http://192.168.90.115:8069/api',
+  baseUrl: 'http://localhost/api',
   type: 'application/json',
   odooToken: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOnsidWlkIjo4LCJhdXRoX21ldGhvZCI6InBhc3N3b3JkIiwibWZhIjoiZGVmYXVsdCJ9LCJleHAiOjE3NzUwNTM5NTUsImlhdCI6MTc3NTA0Njc1NX0.n4y38rlxGPzoiVOOQ_12PA4zR0obVGPpys6hsBtL_JI',
   jsonrpc: "2.0",
