7060b042ba0b258cb5aab4d164a1b5badaff7df4
DevNicolo <dev.nicoloagricola@gmail.com>
2026-05-06 09:43:31 +0200

    feat: remove envirorments directory

--- app/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  baseUrl: 'http://192.168.90.115:8069/api',    //LAVORO http://192.168.90.115:8069/api' //CASA http://192.168.10.118:8069/api
-  type: 'application/json',
-  odooToken: 'Bearer ',
-  jsonrpc: "2.0",
-  method: "call",
-  id: 1
-};
